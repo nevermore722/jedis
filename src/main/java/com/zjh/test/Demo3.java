46b10cdd38c55b93764ff0d7345793eef02fe179
nevermore722 <45936418+nevermore722@users.noreply.github.com>
2020-03-11 12:13:59 +0800

    :tada: 初始化提交。暂时没有东西要提交了

--- src/main/java/com/zjh/test/Demo3.java
+++ src/main/java/com/zjh/test/Demo3.java
@@ -1,5 +1,9 @@
 package com.zjh.test;
 
+import javax.annotation.Resource;
+import org.junit.jupiter.api.Test;
+import redis.clients.jedis.JedisCluster;
+
 /**
  * @author ：ZouJiaHui
  * @date ：Created in 2020/3/10 22:32
@@ -9,4 +13,11 @@ package com.zjh.test;
  */
 public class Demo3 {
 
+  @Resource
+  private JedisCluster jedisCluster;
+
+  @Test
+  public void test(){
+
+  }
 }
